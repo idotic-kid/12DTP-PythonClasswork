af49190a51ad3802010f674bd4b833a71730157e
idotic-kid <em21236@my.westlake.school.nz>
2024-02-15 14:20:58 +1300

    coded in bankmanagement.py

---
+++ bankmanagement.py
@@ -0,0 +1,43 @@
+user = "bob"
+account_balance = 1200
+
+def deposit(bal, dep_amount):
+    try:
+        return int(bal) + int(dep_amount)
+    except:
+        return "fail"
+
+def withdraw(bal, withdraw):
+    if int(withdraw) <= int(bal):
+        return bal - withdraw
+    else:
+        return "fail"
+
+def check_balance(bal):
+    return "Your balance is $"+str(bal)
+
+
+while True:
+    prompt = input("hey bob what do u wanna do\n[1] deposit\n[2] withdraw\n[3] check balance\n")
+
+    if prompt =="1":
+        temp = deposit(account_balance, input("how much would you like to deposit?\n"))
+        if not "fail" in temp:
+            account_balance=temp
+            print("Transaction successful. Balance updated to $"+str(account_balance))
+        else:
+            print("Transaction failed")
+
+    elif prompt=="2":
+        temp = withdraw(account_balance, input("how much would you like to withdraw?\n"))
+        if not "fail" in temp:
+            account_balance=temp
+            print("Transaction successful. Balance updated to $"+str(account_balance))
+        else:
+            print("Transaction failed.")
+
+    elif prompt=="3":
+        print(check_balance(account_balance))
+
+    else:
+        print("Invalid option.")
