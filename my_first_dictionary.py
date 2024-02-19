d3c16b4c9184ba1fc55ca5431243251d2949c909
idotic-kid <em21236@my.westlake.school.nz>
2024-02-20 09:56:45 +1300

    5e6uj6er7jr67j

---
+++ my_first_dictionary.py
@@ -0,0 +1,5 @@
+# Note: this is not my first dictionary
+
+harry_potter = {"Age":"15","Address":"Number 4, Privet Drive","House":"Gryffindor","Subjects":["Transfiguration", "Charms","Potions","History of Magic","Defence Against the Dark Arts", "Astronomy", "Herbology"],"House Points":155}
+print(harry_potter)
+print(f"Hello I am in {harry_potter[House]} house, but I live at {harry_potter[Address]}")
