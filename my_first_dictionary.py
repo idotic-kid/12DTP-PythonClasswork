5527128540caaca7239982c5c65c8734533499cf
idotic-kid <em21236@my.westlake.school.nz>
2024-02-20 10:03:05 +1300

    added something the teacher made me do

--- my_first_dictionary.py
+++ my_first_dictionary.py
@@ -2,4 +2,4 @@
 
 harry_potter = {"Age":"15","Address":"Number 4, Privet Drive","House":"Gryffindor","Subjects":["Transfiguration", "Charms","Potions","History of Magic","Defence Against the Dark Arts", "Astronomy", "Herbology"],"House Points":155}
 print(harry_potter)
-print(f"Hello I am in {harry_potter[House]} house, but I live at {harry_potter[Address]}")
+print("Hello I am in {} house, but I live at {}".format(harry_potter["House"], harry_potter["Address"]))
