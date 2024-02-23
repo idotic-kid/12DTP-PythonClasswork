cd9bfc59ba07902f26cc2082bfa8a2c5c0fa785d
idotic-kid <em21236@my.westlake.school.nz>
2024-02-23 14:20:44 +1300

    added more functions to it

--- shopping_list.py
+++ shopping_list.py
@@ -1,3 +1,15 @@
+def validate_str_to_float(question):
+    i = False
+    question_marks = "?"
+    while i == False:
+        i_str = input(f"{question}{question_marks}\n> ")
+        try:
+            i = float(i_str)
+        except:
+            print("I meant like a NUMBER (in dollars)")
+            question_marks = question_marks+"?"
+    return i
+
 def add_item(list):
     price_float = 69420
     item = input("What item would you like to add?\n> ").lower()
@@ -5,13 +17,7 @@ def add_item(list):
     if item in list:
         return "n/a"
     else:
-        while price_float == 69420:
-            price_str = input(f"How much does {item} cost{question_marks}\n> ")
-            try:
-                price_float = float(price_str)
-            except:
-                print("I meant like a NUMBER (in dollars)")
-                question_marks = question_marks+"?"
+        price_float = validate_str_to_float(f"How much does {item} cost")
         return item, price_float
 
 def total_price(list):
@@ -23,11 +29,26 @@ def find_item(item, list):
     else:
         return False
 
+def display_list(list, isrange):
+    if isrange:
+        minimum_price = validate_str_to_float("Enter minimum price:\n> ")
+        maximum_price = validate_str_to_float("Enter maximum price:\n> ")
+    
+    print("⟪ ꜱʜᴏᴘᴘɪɴɢ ʟɪꜱᴛ ⟫")
+    if len(list) != 0:
+        for i in list:
+            if isrange:
+                if list[i] > minimum_price and list[i] < maximum_price:
+                    print(" - {}: ${}".format(i, list[i]))
+            else:
+                print(" - {}: ${}".format(i, list[i]))
+    else:
+        print("There are no items in your shopping list.")
 
 shopping_list = {}
 
 while True:
-    menu_option = input("What would you like to do?\n[1] Add one item to shopping list\n[2] Calculate total price of shopping list\n[3] Check price of item in list\n> ")
+    menu_option = input("What would you like to do?\n[1] Add one item to shopping list\n[2] Calculate total price of shopping list\n[3] Check price of item in list\n[4] Display full shopping list\n[5] Display items within a specified price range\n> ")
     
     if menu_option == "1":
         new_item = add_item(shopping_list)
@@ -47,5 +68,10 @@ while True:
         else:
             print("That item is not in your shopping list.")
     
+    elif menu_option =="4":
+        display_list(shopping_list, False)
+    
+    elif menu_option=="5":
+        display_list(shopping_list, True)
     else:
         print("Invalid option.")
