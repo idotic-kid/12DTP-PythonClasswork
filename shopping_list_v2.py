2240f126b06173fae36a8c3a84f5f60f1ffad9b6
idotic-kid <em21236@my.westlake.school.nz>
2024-02-27 12:00:12 +1300

    aaaaaaaaaaaaaaaaaaa

--- shopping_list_v2.py
+++ shopping_list_v2.py
@@ -6,16 +6,41 @@ def validate_str_to_float(question):
         try:
             i = float(i_str)
         except:
-            print("I meant like a NUMBER (in dollars)")
+            print("I meant like a NUMBER")
             question_marks = question_marks+"?"
     return i
 
-def add_item(list):
+def add_item(list, mod):
+
+    # "mod" is a parameter that stores the menu output, being
+    #[1] add [2] delete [3] change price [4] change quantity
+    # operation_real is set to this so that the value can be changed later
+
+    # Creates a temporary list variable with the uncommited modifications
+    # Is always set to the original shopping list but ig if you wanted to have multiple shopping lists lol
+
     templist = list
-    print(templist)
-    #remember to .lower() the input item name
-    # list[input("item name").lower()] = {"price":validate_str_to_float(),"quantity":validate_str_to_float_()}
-    return
+    
+    new_item = input("Enter the name of this item\n> ").lower()
+
+
+    if mod == "1" and new_item in list:
+        print("This item is already in your list. Perhaps you meant to do something else?")
+        print("""[1] Back out to main menu
+[2] Remove the item from the shopping list
+[3] Change the price of the item
+[4] Change the quantity of the item""")
+        djfhfbdh = input("> ")
+
+
+    else:
+        operation_real = mod
+
+
+    templist[new_item] = {"price":validate_str_to_float("How much does it cost"),"quantity":validate_str_to_float("How much of this item do you want to add")}
+    return templist
+
+
 
 def total_price(list):
     total = 0
@@ -51,26 +76,27 @@ shopping_list = {"banana":{"price":3.5,"quantity":4}}
 
 
 while True:
-    menu_option = input("What would you like to do?\n[1] Add one item to shopping list\n[2] Calculate total price of shopping list\n[3] Check price of item in list\n[4] Display full shopping list\n[5] Display items within a specified price range\n> ")
+    menu_option = input("What would you like to do?\n[1] Modify your shopping list\n[2] View from your shopping list\n> ")
     
-    if menu_option == "1":
-        print("no code here yet")
-        shopping_list = add_item(shopping_list)
+    if menu_option == "1": # Path for writing to list
+        while menu_option !="5":
+            menu_option = input("""[<] Modify your shopping list
+Options:
+    [1] Add an item to your shopping list
+    [2] Remove an item from your shopping list
+    [3] Change the price of an item
+    [4] Change the quantity of an item
+    > """)
+            if menu_option in "1234":
+                add_item(shopping_list, menu_option)
+            else:
+                print("Bad input")
+            
+            
 
-    elif menu_option == "2":
-        print("Total price is $", total_price(shopping_list))
-    
-    elif menu_option =="3":
-        item_price = find_item(input("Which item do you want to find?\n> "), shopping_list)
-        if item_price != False:
-            print("that item has a price of $", item_price)
-        else:
-            print("That item is not in your shopping list.")
-    
-    elif menu_option =="4":
-        display_list(shopping_list, False)
-    
-    elif menu_option=="5":
-        display_list(shopping_list, True)
+    elif menu_option == "2": # Path for reading from list
+        menu_option = input("")
+
+
     else:
         print("Invalid option.")
