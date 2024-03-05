bb1087a8605cda42f0d90d1ac30a90b8adb97946
idotic-kid <em21236@my.westlake.school.nz>
2024-03-06 12:00:43 +1300

    oh man this sucks im gonna redo the ENTIRE thing at home

--- movie_theatre.py
+++ movie_theatre.py
@@ -1,29 +1,53 @@
 from easygui import *
 
-
-movies = {"The Shawshank Redemption":["Drama", 142, "22:00", 143],
-"The Godfather":["Drama", 175, "15:30", 111],
-"Back to the Future":["Comedy", 116, "12:00", 102],
-"Spirited Away":["Family", 125, "9:30", 98],
-"The Lion King":["Family", 125, "07:00", 123]}
-
-
-options = {"Edit Genre":0, "Edit Duration":1,
-    "Schedule showtime":2, "Edit bookings":3}
-
-
-def menu_loop():
-    option_that_was_selected = 6996874397549
-    while option_that_was_selected != "Exit":
-        pass
+def function_that_checks_whether_something_is_in_24h_format():
+    cOCKaNDbALLtORTURE = True
+    string_of_numbers = "0123456789"
+    while cOCKaNDbALLtORTURE:
+        failture=0
+        penisanusimagine = enterbox("Enter when the show is in hh:mm!")
+        for i in penisanusimagine:
+            if i not in string_of_numbers and i!=":":
+                msgbox("NO YOU BALLSACK grr")
+                break
+                       
     pass
 
-def edit_movie(genre=None, dur=None, showtime=None, bookings=None):
-    '''This will edit at least one item of the list in the value thing'''
+def add_movie(movies_dictionary):
+    #this might work if it works dont touch it cuz i dont remember
+    selected_movie = enterbox("What movie do you want to add?")
+    if selected_movie in movies_dictionary:
+        msgbox("You already have that movie, silly!")
+    else:
+        edit_movie(selected_movie, movies_dictionary,
+        genre=enterbox("Enter what genre!!", "add genre"),
+        dur=integerbox("Enter how long the movie is(minutes)!", lowerbound=0,),
+        showtime=function_that_checks_whether_something_is_in_24h_format(),
+        bookings=integerbox("How many bookings are there?",
+    "Edit > Bookings", lowerbound=0, upperbound=150)     )
+
+
+        buttonbox(choices=["yah", "nah"])
+
+    pass
+
+
+def edit_movie(movie, movies_dictionary,
+genre=None, dur=None, showtime=None, bookings=None):
+    temp_dick = movies_dictionary
+    temp_dick[movie] = {genre}
     
-    pass
+    return temp_dick
 
-def edit_bookings():
+def choose_a_movie_from_the_movies_dictionary_list(movies_dictionary,
+what_thing_in_the_movie_you_wanna_edit):
+    penisballssexamongus = choicebox(f"Which movie do you wanna \
+        edit the {what_thing_in_the_movie_you_wanna_edit} of?", "Choosing",
+        list(movies_dictionary.keys()))
+    return penisballssexamongus
+
+
+def edit_bookings(movies_dictionary):
     """ ask user for a number
     if the output is over 150 then make you do it again
     then edit"""
@@ -32,9 +56,45 @@ def edit_bookings():
     "Edit > Bookings", lowerbound=0, upperbound=150))
     pass
 
-def search():
+
+def edit_showtime(movies_dictionary):
+    '''same thng as editbooking but with idx 3 instead of idx2'''
+    msgbox("edit showtime or something")
     pass
 
 
+def search(movies_dictionary):
+    msgbox("search or something idk")
+    pass
 
-menu_loop()
+
+def check_with_user(movies_dictionary):
+    pass
+
+
+movies = {"The Shawshank Redemption":["Drama", 142, "22:00", 143],
+"The Godfather":["Drama", 175, "15:30", 111],
+"Back to the Future":["Comedy", 116, "12:00", 102],
+"Spirited Away":["Family", 125, "9:30", 98],
+"The Lion King":["Family", 125, "07:00", 123]}
+
+
+options_menu_list_of_options = {
+    "Add a new movie":add_movie,
+    "Search for a movie":search,
+    "Reschedule a showtime":edit_showtime,
+    "Change the number of bookings for the movie":edit_bookings,
+    "Exit":exit
+    }
+
+
+
+
+#mainloop
+while True:
+    selected_option = buttonbox("What would you like to do",
+    "select smt", list(options_menu_list_of_options.keys()))
+    
+    movies = options_menu_list_of_options[selected_option](movies)
+
+
